5aab01402f37e293eba8554ba75398c6e80ff1b9
948108316 <948108316@qq.com>
2016-12-26 11:54:41 +0800

    2

---
+++ app/src/main/java/com/jfq/sms/entity/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.jfq.sms.entity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jfq.sms.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
